967c4b4761045c8d86516aea9cd9b6ec55b10b3c
tundetaiwo <dantaiwo96@gmail.com>
2024-10-21 00:38:58 +0100

    add: bigger terminal boxes

--- nvim/chadrc.lua
+++ nvim/chadrc.lua
@@ -14,4 +14,16 @@ M.base46 = {
   -- }
 }
 
+  M.term = {
+    winopts = { number = false, relativenumber = false },
+    sizes = { sp = 0.5, vsp = 0.4, ["bo sp"] = 0.3, ["bo vsp"] = 0.2 },
+    float = {
+      relative = "editor",
+      row = 0.1,
+      col = 0.15,
+      width = 0.75,
+      height = 0.75,
+      border = "single",
+    },
+  }
 return M
