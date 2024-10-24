c83295a8bb4506f190fc03ed3f879f8e0f00e068
tundetaiwo <dantaiwo96@gmail.com>
2024-10-24 22:10:10 +0100

    add: options settings for configuration

--- nvim/options.lua
+++ nvim/options.lua
@@ -4,3 +4,7 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+vim.g.vscode_snippets_path = "/Users/tundetaiwo/.config/nvim/lua/snippets"
+vim.opt.relativenumber = true
+vim.opt.clipboard = ""
