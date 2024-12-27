9743358f764af4e7243a53b8903b942a6e0f8c17
tundetaiwo <dantaiwo96@gmail.com>
2024-12-27 17:32:39 +0000

    refactor: make theme catppuccin

--- nvim/lua/ui.lua
+++ nvim/lua/ui.lua
@@ -1,15 +1,11 @@
--- Chose Theme
+-- Theme Settings
+require("catppuccin").setup({
+    flavour = "mocha",
+    -- flavour = "macchiato",
+    -- flavour = "frappe",
+    transparent_background = true,
+    how_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+  })
+
+-- Set Theme
 vim.cmd.colorscheme "catppuccin"
-
--- Make empty lines after end of file have " " instead of "~"
--- vim.o.fillchars = "eob: "
-
--- Make Background Transparent
-vim.cmd([[ 
-    hi Normal guibg=NONE ctermbg=NONE
-    hi NormalNC guibg=NONE ctermbg=NONE
-    hi EndOfBuffer guibg=NONE ctermbg=NONE
-    hi SignColumn guibg=NONE ctermbg=NONE
-    hi NvimTreeNormal guibg=NONE ctermbg=NONE
-    "hi ToggleTermVeritcal guibg=NONE ctermbg=NONE
-]])
