708226371ded54ffa200dd4fa8886fda57854eb4
tundetaiwo <dantaiwo96@gmail.com>
2024-12-28 02:12:01 +0000

    add: new startup for TuniVim

---
+++ nvim/lua/configs/alpha.lua
@@ -0,0 +1,60 @@
+local alpha = require('alpha')
+local dashboard = require('alpha.themes.dashboard')
+dashboard.section.header.val = {
+
+[[  '########:'##::::'##:'##::: ##:'####:'##::::'##:'####:'##::::'##:  ]],
+[[  ... ##..:: ##:::: ##: ###:: ##:. ##:: ##:::: ##:. ##:: ###::'###:  ]],
+[[  ::: ##:::: ##:::: ##: ####: ##:: ##:: ##:::: ##:: ##:: ####'####:  ]],
+[[  ::: ##:::: ##:::: ##: ## ## ##:: ##:: ##:::: ##:: ##:: ## ### ##:  ]],
+[[  ::: ##:::: ##:::: ##: ##. ####:: ##::. ##:: ##::: ##:: ##. #: ##:  ]],
+[[  ::: ##:::: ##:::: ##: ##:. ###:: ##:::. ## ##:::: ##:: ##:.:: ##:  ]],
+[[  ::: ##::::. #######:: ##::. ##:'####:::. ###::::'####: ##:::: ##:  ]],
+[[  :::..::::::.......:::..::::..::....:::::...:::::....::..:::::..:  ]],
+}
+
+
+-- Position Header
+dashboard.config.layout = {
+  { type = "padding", val = 15 }, -- Adjust padding to push it down
+  dashboard.section.header,
+  { type = "padding", val = 2 }, -- Additional padding after the header
+  dashboard.section.buttons,
+}
+
+
+local new_file_but = dashboard.button( "e", "  New file" , ":ene <BAR> startinsert <CR>")
+local close_nvim_but = dashboard.button( "q", "󰅚  Quit NVIM" , ":qa<CR>")
+local file_file_but = dashboard.button( "Ctrl + p", "  Find File" , ":Telescope find_files ")
+local last_sess_butt = dashboard.button( "SPC s l", "  Open Last Session" , ":Telescope find_files ")
+
+dashboard.section.buttons.val = {
+  new_file_but,
+   close_nvim_but,
+   file_file_but,
+   last_sess_butt,
+}
+
+-- Set Colours
+vim.api.nvim_set_hl(0, "DashboardColours", { fg = "#a3a6c2" })
+
+dashboard.section.header.opts.hl = "DashboardColours"
+
+new_file_but.opts.hl = "DashboardColours"
+new_file_but.opts.hl_shortcut = "DashboardColours"
+close_nvim_but.opts.hl = "DashboardColours"
+close_nvim_but.opts.hl_shortcut = "DashboardColours"
+file_file_but.opts.hl = "DashboardColours"
+file_file_but.opts.hl_shortcut = "DashboardColours"
+last_sess_butt.opts.hl = "DashboardColours"
+last_sess_butt.opts.hl_shortcut = "DashboardColours"
+
+local handle = io.popen('fortune')
+local fortune = handle:read("*a")
+handle:close()
+dashboard.section.footer.val = fortune
+
+dashboard.config.opts.noautocmd = true
+
+vim.cmd[[autocmd User AlphaReady echo 'ready']]
+
+alpha.setup(dashboard.config)
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -186,5 +186,20 @@ local plugins = {
     dir = plugin_folder .. "catppuccin-nvim",
     priority=1000,
     }
+  -- Custom Startup
+  {
+    "goolord/alpha-nvim",
+    dependencies = { dir = plugin_folder .. "nvim-web-devicons" },
+    config = function()
+      local startify = require("alpha.themes.startify")
+      -- available: devicons, mini, default is mini
+      -- if provider not loaded and enabled is true, it will try to use another provider
+      startify.file_icons.provider = "devicons"
+      require("alpha").setup(
+        startify.config
+      )
+      require("configs.alpha")
+    end,
+  },
 }
 return plugins
