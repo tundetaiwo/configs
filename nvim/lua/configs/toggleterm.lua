f7eda9e1a719c334370d81f685aefede74458c3a
tundetaiwo <dantaiwo96@gmail.com>
2025-02-16 23:07:26 +0000

    refactor: toggleterm scroll within floating window

--- nvim/lua/configs/toggleterm.lua
+++ nvim/lua/configs/toggleterm.lua
@@ -1,50 +1,53 @@
 local Terminal = require('toggleterm.terminal').Terminal
 
+require('toggleterm').setup({
+	on_open = function(term)
+		vim.api.nvim_buf_set_keymap(term.bufnr, 't', '<C-u>', '<C-\\><C-n><C-d>i', { noremap = true })
+	end,
+	on_enter = function()
+		vim.cmd('startinsert!')
+	end,
+})
+
 -- Helper function to check if ipython is available
 local function check_ipython()
-    local handle = io.popen("which ipython 2>/dev/null")
-    if handle then
-        local result = handle:read("*a")
-        handle:close()
-        return result ~= ""
-    end
-    return false
+	local handle = io.popen("which ipython 2>/dev/null")
+	if handle then
+		local result = handle:read("*a")
+		handle:close()
+		return result ~= ""
+	end
+	return false
 end
 
 -- Helper function to show error message
 local function show_error_message()
-    vim.api.nvim_err_writeln("Error: IPython is not available in your environment.\n" ..
-                            "Please install IPython or activate your virtual environment.")
+	vim.api.nvim_err_writeln("Error: IPython is not available in your environment.\n" ..
+		"Please install IPython or activate your virtual environment.")
 end
 
 function _window_toggle(window)
-    -- Special handling for interactive window
-    if window == interactive_window and not check_ipython() then
-        show_error_message()
-        return
-    end
-    
-    window:toggle()
-    -- change to insert mode when toggling
-    if window:is_open() then
-        window:set_mode("i")
-    end
-end
+	-- Special handling for interactive window
+	if window == interactive_window and not check_ipython() then
+		show_error_message()
+		return
+	end
 
-require('toggleterm').setup({
-    on_enter = function()
-        vim.cmd('startinsert!')
-    end
-})
+	window:toggle()
+	-- change to insert mode when toggling
+	if window:is_open() then
+		window:set_mode("i")
+	end
+end
 
 -- Float Window --
 local float_window = Terminal:new({
-    direction = "float",
-    float_opts = {
-        border = "double",
-        size = 40,
-    },
-    close_on_exit = true,
+	direction = "float",
+	float_opts = {
+		border = "double",
+		size = 40,
+	},
+	close_on_exit = true,
 })
 
 vim.keymap.set("n", "<A-i>", function() _window_toggle(float_window) end, { noremap = true, silent = true })
@@ -52,8 +55,8 @@ vim.keymap.set("t", "<A-i>", function() _window_toggle(float_window) end, { nore
 
 -- Vertical Window --
 local vertical_window = Terminal:new({
-    direction = "vertical",
-    close_on_exit = true,
+	direction = "vertical",
+	close_on_exit = true,
 })
 vertical_window:resize(30)
 vim.keymap.set("n", "<A-v>", function() _window_toggle(vertical_window) end, { noremap = true, silent = true })
@@ -61,8 +64,8 @@ vim.keymap.set("t", "<A-v>", function() _window_toggle(vertical_window) end, { n
 
 -- Horizontal Window --
 local horizontal_window = Terminal:new({
-    direction = "horizontal",
-    close_on_exit = true,
+	direction = "horizontal",
+	close_on_exit = true,
 })
 
 vim.keymap.set("n", "<A-h>", function() _window_toggle(horizontal_window) end, { noremap = true, silent = true })
@@ -70,14 +73,14 @@ vim.keymap.set("t", "<A-h>", function() _window_toggle(horizontal_window) end, {
 
 -- Lazy Git --
 local lazygit = Terminal:new({
-    display_name = "Lazy Git",
-    cmd = "lazygit",
-    dir = "git_dir",
-    direction = "float",
-    float_opts = {
-        border = "double",
-        title_pos = "center"
-    },
+	display_name = "Lazy Git",
+	cmd = "lazygit",
+	dir = "git_dir",
+	direction = "float",
+	float_opts = {
+		border = "double",
+		title_pos = "center"
+	},
 })
 
 vim.keymap.set("n", "<A-g>", function() _window_toggle(lazygit) end, { noremap = true, silent = true })
@@ -86,19 +89,18 @@ vim.keymap.set("t", "<A-g>", function() _window_toggle(lazygit) end, { noremap =
 -- Interactive Window --
 local interactive_window
 if check_ipython() then
-    interactive_window = Terminal:new({
-        cmd = "ipython --autoindent",
-        display_name = "Interactive Window",
-        direction = "vertical",
-        size = function()
-            return vim.o.columns * 0.5
-        end,
-        on_open = function(term)
-            term:resize(vim.o.columns * 0.5)
-        end
-    })
+	interactive_window = Terminal:new({
+		cmd = "ipython --autoindent",
+		display_name = "Interactive Window",
+		direction = "vertical",
+		size = function()
+			return vim.o.columns * 0.5
+		end,
+		on_open = function(term)
+			term:resize(vim.o.columns * 0.5)
+		end
+	})
 end
 
 vim.keymap.set("n", "<A-p>", function() _window_toggle(interactive_window) end, { noremap = true, silent = true })
 vim.keymap.set("t", "<A-p>", function() _window_toggle(interactive_window) end, { noremap = true, silent = true })
-
