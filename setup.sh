d783888a5aced4497158dca33b7faad991de4b30
tundetaiwo <dantaiwo96@gmail.com>
2025-02-22 16:51:52 +0000

    add: lua language server to setup

--- setup.sh
+++ setup.sh
@@ -22,9 +22,14 @@ sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 rm nvim-linux-x86_64.tar.gz
 
+# Lua Language Server
+curl -LO https://github.com/LuaLS/lua-language-server/releases/download/3.13.6/lua-language-server-3.13.6-linux-x64.tar.gz
+tar -C $HOME/.local/share/lua-language-server/ -xzf lua-language-server-3.13.6-linux-x64.tar.gz
+rm lua-language-server-3.13.6-linux-x64.tar.gz
+
 # Install lazygit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+curl -LO lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit -D -t /usr/local/bin/
 rm lazygit.tar.gz
