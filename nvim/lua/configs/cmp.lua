2879c0fae37e9bd41d691c5adac102423ac150c4
tundetaiwo <dantaiwo96@gmail.com>
2025-02-22 17:02:06 +0000

    fix: structure of cmp config

--- nvim/lua/configs/cmp.lua
+++ nvim/lua/configs/cmp.lua
@@ -1,62 +1,58 @@
 local cmp = require "cmp"
 
-cmp.setup.cmdline('/', {
-	mapping = {
-		cmp.mapping.preset.cmdline()
-	},
-	sources = {
-		{ name = 'buffer' }
-	}
+local options = {
+    completion = { completeopt = "menu,menuone" },
+    snippet = {
+        expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+        end,
+    },
+    mapping = {
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.close(),
+        ["<CR>"] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true,
+        },
+    },
+    sources = {
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "nvim_lua" },
+        { name = "path" },
+        { name = "cmdline" },
+    },
+}
+
+-- Apply the main setup first
+cmp.setup(options)
+
+-- Then configure cmdline completions
+cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' }
+    }
 })
 
 cmp.setup.cmdline(':', {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = cmp.config.sources({
-		{ name = 'path' }
-	}, {
-		{
-			name = 'cmdline',
-			option = {
-				ignore_cmds = { "Man", "!" }
-			}
-		}
-	}),
-	matching = { disallow_symbol_nonprefix_matching = false }
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+    }, {
+        {
+            name = 'cmdline',
+            option = {
+                ignore_cmds = { "Man", "!" }
+            }
+        }
+    }),
+    matching = { disallow_symbol_nonprefix_matching = false }
 })
-
-
-local options = {
-	completion = { completeopt = "menu,menuone" },
-
-	snippet = {
-		expand = function(args)
-			require("luasnip").lsp_expand(args.body)
-		end,
-	},
-
-	mapping = {
-		["<C-k>"] = cmp.mapping.select_prev_item(),
-		["<C-j>"] = cmp.mapping.select_next_item(),
-		["<C-d>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<C-e>"] = cmp.mapping.close(),
-
-		["<CR>"] = cmp.mapping.confirm {
-			behavior = cmp.ConfirmBehavior.Insert,
-			select = true,
-		},
-	},
-
-
-	sources = {
-		{ name = "nvim_lsp" },
-		{ name = "luasnip" },
-		{ name = "buffer" },
-		{ name = "nvim_lua" },
-		{ name = "path" },
-		{ name = "cmdline" },
-	},
-}
 
 return options
