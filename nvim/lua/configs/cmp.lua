507bd1b93066cb7892af481014e14e13aa7ebae5
tundetaiwo <dantaiwo96@gmail.com>
2025-02-22 16:28:20 +0000

    refactor: cmp mappings

--- nvim/lua/configs/cmp.lua
+++ nvim/lua/configs/cmp.lua
@@ -1,14 +1,14 @@
 local cmp = require "cmp"
 
--- `/` cmdline setup.
 cmp.setup.cmdline('/', {
-	mapping = cmp.mapping.preset.cmdline(),
+	mapping = {
+		cmp.mapping.preset.cmdline()
+	},
 	sources = {
 		{ name = 'buffer' }
 	}
 })
 
--- `:` cmdline setup.
 cmp.setup.cmdline(':', {
 	mapping = cmp.mapping.preset.cmdline(),
 	sources = cmp.config.sources({
@@ -17,63 +17,46 @@ cmp.setup.cmdline(':', {
 		{
 			name = 'cmdline',
 			option = {
-				ignore_cmds = { 'Man', '!' }
+				ignore_cmds = { "Man", "!" }
 			}
 		}
-	})
+	}),
+	matching = { disallow_symbol_nonprefix_matching = false }
 })
 
+
 local options = {
-  completion = { completeopt = "menu,menuone" },
+	completion = { completeopt = "menu,menuone" },
 
-  snippet = {
-    expand = function(args)
-      require("luasnip").lsp_expand(args.body)
-    end,
-  },
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
 
-  mapping = {
-    ["<C-k>"] = cmp.mapping.select_prev_item(),
-    ["<C-j>"] = cmp.mapping.select_next_item(),
-    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-e>"] = cmp.mapping.close(),
+	mapping = {
+		["<C-k>"] = cmp.mapping.select_prev_item(),
+		["<C-j>"] = cmp.mapping.select_next_item(),
+		["<C-d>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.close(),
 
-    ["<CR>"] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Insert,
-      select = true,
-    },
+		["<CR>"] = cmp.mapping.confirm {
+			behavior = cmp.ConfirmBehavior.Insert,
+			select = true,
+		},
+	},
 
-    ["<C-j>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif require("luasnip").expand_or_jumpable() then
-        require("luasnip").expand_or_jump()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
 
-    ["<C-j>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif require("luasnip").jumpable(-1) then
-        require("luasnip").jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-  },
-
-  sources = {
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-    { name = "buffer" },
-    { name = "nvim_lua" },
-    { name = "path" },
-    { name = "cmdline" },
-  },
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+		{ name = "buffer" },
+		{ name = "nvim_lua" },
+		{ name = "path" },
+		{ name = "cmdline" },
+	},
 }
 
 return options
