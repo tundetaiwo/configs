138752bc9baff47c867ade7b5e9fb5058d5564f1
tundetaiwo <dantaiwo96@gmail.com>
2025-03-27 09:31:18 +0000

    refactor: change keybinds of command-mode complete from tab to ctrl+j/k

--- nvim/lua/configs/cmp.lua
+++ nvim/lua/configs/cmp.lua
@@ -1,58 +1,93 @@
 local cmp = require "cmp"
 
 local options = {
-    completion = { completeopt = "menu,menuone" },
-    snippet = {
-        expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-        end,
-    },
-    mapping = {
-        ["<C-k>"] = cmp.mapping.select_prev_item(),
-        ["<C-j>"] = cmp.mapping.select_next_item(),
-        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.close(),
-        ["<CR>"] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Insert,
-            select = true,
-        },
-    },
-    sources = {
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-        { name = "buffer" },
-        { name = "nvim_lua" },
-        { name = "path" },
-        { name = "cmdline" },
-    },
+	completion = { completeopt = "menu,menuone" },
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
+	mapping = {
+		["<C-k>"] = cmp.mapping.select_prev_item(),
+		["<C-j>"] = cmp.mapping.select_next_item(),
+		["<C-d>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.close(),
+		["<CR>"] = cmp.mapping.confirm {
+			behavior = cmp.ConfirmBehavior.Insert,
+			select = true,
+		},
+	},
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+		{ name = "buffer" },
+		{ name = "nvim_lua" },
+		{ name = "path" },
+		{ name = "cmdline" },
+	},
 }
 
 -- Apply the main setup first
 cmp.setup(options)
 
--- Then configure cmdline completions
-cmp.setup.cmdline({ '/', '?' }, {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-        { name = 'buffer' }
-    }
+-- Command-line (search "/" and "?") completion setup
+cmp.setup.cmdline({ "/", "?" }, {
+	mapping = cmp.mapping.preset.cmdline({
+		["<Tab>"]   = { c = function() end },  -- Disable Tab in cmdline mode
+		["<S-Tab>"] = { c = function() end },  -- Disable Shift-Tab in cmdline mode
+		["<C-j>"]   = {
+			c = function(fallback)               -- Ctrl+j: next item
+				if cmp.visible() then
+					cmp.select_next_item()
+				else
+					fallback()
+				end
+			end
+		},
+		["<C-k>"]   = {
+			c = function(fallback)               -- Ctrl+k: previous item
+				if cmp.visible() then
+					cmp.select_prev_item()
+				else
+					fallback()
+				end
+			end
+		},
+	}),
+	sources = {
+		{ name = "buffer" }
+	}
 })
 
-cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-        { name = 'path' }
-    }, {
-        {
-            name = 'cmdline',
-            option = {
-                ignore_cmds = { "Man", "!" }
-            }
-        }
-    }),
-    matching = { disallow_symbol_nonprefix_matching = false }
+-- Command-line (":") completion setup
+cmp.setup.cmdline(":", {
+	mapping = cmp.mapping.preset.cmdline({
+		["<Tab>"]   = { c = function() end }, -- Disable Tab in cmdline mode
+		["<S-Tab>"] = { c = function() end }, -- Disable Shift-Tab in cmdline mode
+		["<C-j>"]   = {
+			c = function(fallback)            -- Ctrl+j: next item
+				if cmp.visible() then
+					cmp.select_next_item()
+				else
+					fallback()
+				end
+			end
+		},
+		["<C-k>"]   = {
+			c = function(fallback) -- Ctrl+k: previous item
+				if cmp.visible() then
+					cmp.select_prev_item()
+				else
+					fallback()
+				end
+			end
+		},
+	}),
+	sources = cmp.config.sources({
+		{ name = "path" }
+	}, {
+		{ name = "cmdline" }
+	})
 })
-
-return options
