35dc54aad32f88ab9529ca0349a182e6e47d1f46
tundetaiwo <dantaiwo96@gmail.com>
2026-07-19 00:56:19 +0100

    add markdown renderer

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -234,6 +234,15 @@ local plugins = {
 			require("configs.csvview")
 		end
 	},
+	{
+    dir = plugin_folder .. "render-markdown.nvim/",
+    dependencies = {{ dir = plugin_folder .. "nvim-treesitter" }, {dir = plugin_folder .. "mini.nvim" }},            -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+	},
 	-- Vim slime (Sends to REPL)
 	{
 		dir = plugin_folder .. "vim-slime",
